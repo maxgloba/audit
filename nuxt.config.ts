4c4c96bf0588f4f62ece0e0243189cada1499920
Max Globa <onyx18121990@gmail.com>
2025-06-16 15:15:07 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,14 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   ssr: false,
   app: {
+    baseURL: '/audit/',
     head: {
       htmlAttrs: { lang: 'ru' },
       link: [
         { rel: "icon", href: "/favicon.ico", sizes: "any" }
       ],
       charset: 'utf-8',
-      viewport: 'width=device-width, user-scalable=no, user-scalable=0, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0 user-scalable=0',
+      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no',
       meta: [
         { name: 'description', content: '' },
         { name: 'keywords', content: '' },
@@ -25,13 +26,14 @@ export default defineNuxtConfig({
     'vue3-carousel-nuxt',
     '@nuxt/image',
     ['@nuxtjs/google-fonts', {
-        families: {
-          'Roboto Condensed': true,
-          'Source Serif 4': true,
-        }
+      families: {
+        'Roboto Condensed': true,
+        'Source Serif 4': true,
+      }
     }],
   ],
   image: {
+    provider: 'static',
     quality: 80,
     format: ['webp'],
     screens: {
@@ -40,7 +42,6 @@ export default defineNuxtConfig({
       md: 768,
       lg: 1024,
       xl: 1280,
-      xxl: 1536,
       '2xl': 1536
     },
   },
@@ -62,40 +63,15 @@ export default defineNuxtConfig({
   css: ['~/assets/styles.css'],
   compatibilityDate: '2025-07-16',
   hooks: {
-    "builder:watch": console.log
+    'builder:watch'(event) {
+      console.log(event);
+    }
   },
   typescript: {
     typeCheck: false,
     strict: false
   },
-  router: {
-    base: '/audit/',
-    mode: 'history',
-    extendRoutes(routes, resolve) {
-      routes.push({
-        name: 'error',
-        path: '*',
-        component: resolve(__dirname, 'app.vue'),
-      })
-    },
-  },
-  generate: {
-    dir: './dist',
-  },
-  build: {
-    target: 'static',
-    extend(config, ctx) {
-      config.module.rules.push({
-        test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      })
-      config.module.rules.push({
-        test: /\.(glsl|vs|fs|vert|frag)$/,
-        loader: 'raw-loader',
-      })
-    },
+  nitro: {
+    preset: 'static'
   },
 })
